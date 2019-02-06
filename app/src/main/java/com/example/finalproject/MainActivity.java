a31ce3fb0341b50fb9129f4a2fa6b0091ba25332
jay9297 <38008607+jay9297@users.noreply.github.com>
2019-02-06 12:02:08 -0600

    Edited some documentation and re-did the onclick listener. Also added a weather button on the main page for testing purposes.

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -6,30 +6,50 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        // Creates the activity upon starting the app
+        /* Creates the activity upon starting the app */
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
 
-        // Create our sign-up button as an object
+        /* Buttons */
+        // Create our sign-up and login buttons as an objects
         Button signUp = (Button) findViewById(R.id.sign_up);
+        Button login = (Button) findViewById(R.id.log_in);
 
         // Create an onclick listener for our sign-up button to take the user to the sign up page on button click
-        signUp.setOnClickListener(new View.OnClickListener()
+        signUp.setOnClickListener(this);
+
+        // Create an onclick listener for the login button to take the user to the main page
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        // Determine which button was pressed and open the corresponding page
+        switch (v.getId())
         {
-            @Override
-            public void onClick(View v)
+            case R.id.sign_up:
             {
-                // Start the sign up page activity
-                Intent signupIntent = new Intent(MainActivity.this, SignUpActivity.class);
-                startActivity(signupIntent);
+                Intent signUp = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(signUp);
+                break;
             }
-        });
+            case R.id.log_in:
+            {
+                Intent mainPage = new Intent(MainActivity.this, MainPage.class);
+                startActivity(mainPage);
+                break;
+            }
+
+            default:
+                break;
+        }
     }
 
 }
