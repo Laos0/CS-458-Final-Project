9533752c5f022116b1de3e08db8775e45c39d92b
Nicholas LaBelle <nrlabelle@gmail.com>
2019-02-06 11:39:57 -0600

    Added weather api code to modify

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.finalproject;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    
 }
