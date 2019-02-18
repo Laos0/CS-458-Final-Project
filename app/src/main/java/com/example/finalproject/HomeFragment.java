e35bb0ba73317842d8fb1f2600324ae057f13cf2
laos0 <isonylao@gmail.com>
2019-02-18 11:51:12 -0600

    Added in the camera feature

--- app/src/main/java/com/example/finalproject/HomeFragment.java
+++ app/src/main/java/com/example/finalproject/HomeFragment.java
@@ -1,20 +1,110 @@
 package com.example.finalproject;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.strictmode.NonSdkApiUsedViolation;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.app.Activity.RESULT_OK;
+import static android.os.Environment.getExternalStoragePublicDirectory;
 
 public class HomeFragment extends Fragment
 {
+    String pathToFile; // path of the image in directory
+    FloatingActionButton cameraBtn;
+    ImageView photo;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_home,container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container,false);
+        cameraBtn = (FloatingActionButton) view.findViewById(R.id.camera_btn);
+        photo = (ImageView)view.findViewById(R.id.photo);
+
+        // for the camera
+        if (Build.VERSION.SDK_INT >= 23) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+        }
+
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Testing to see if take picture button works
+                // String tag = "TESTING";
+                // Log.d(tag, "DISPATCHPICTURETAKERACTION IS BEING RUN");
+                dispatchPictureTakerAction();
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                Bitmap bitmap = BitmapFactory.decodeFile(pathToFile);
+                photo.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    private void dispatchPictureTakerAction() {
+        Intent takePic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        FragmentActivity activity = HomeFragment.this.getActivity();
+        PackageManager packageManager = activity.getPackageManager();
+        if (takePic.resolveActivity(packageManager) != null) {
+            File photoFile = null;
+            photoFile = createPhotoFile();
+
+            if (photoFile != null) {
+                pathToFile = photoFile.getAbsolutePath(); // path to the photo file
+                Uri photoURI = FileProvider.getUriForFile(HomeFragment.this.getActivity(), "com.example.finalproject", photoFile);
+                takePic.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePic, 1);
+            }
+
+        }
+    }
+
+    // Creating the file of the photo
+    private File createPhotoFile() {
+        String pattern = "yyyy-MM-dd";
+        String name = new SimpleDateFormat(pattern).format(new Date());
+        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try {
+            image = File.createTempFile(name, ".jpg", storageDir);
+        } catch (IOException e) {
+            Log.d("myLog", "Excep: " + e.toString());
+        }
+
+        return image;
+
     }
 }
