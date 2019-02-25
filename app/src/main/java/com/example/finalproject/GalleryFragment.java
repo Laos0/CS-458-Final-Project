e95fc5a10c99627d6f23619903dd14702a589e73
roskomj <jordan.roskom@yahoo.com>
2019-02-25 12:24:27 -0600

    jd

--- app/src/main/java/com/example/finalproject/GalleryFragment.java
+++ app/src/main/java/com/example/finalproject/GalleryFragment.java
@@ -18,19 +18,37 @@ import com.bumptech.glide.Glide;
 
 public class GalleryFragment extends Fragment {
 
-    @SuppressLint("StaticFieldLeak")
-    static Context c;
-    @SuppressLint("StaticFieldLeak")
-    static LinearLayout gallery;
+    Context c;
+    LinearLayout gallery;
+    View view;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+        view = inflater.inflate(R.layout.fragment_gallery, container, false);
         c = this.getContext();
         gallery = view.findViewById(R.id.LinearP);
+
+        gallery.addView(makeGallery(55));
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (view != null) {
+            ViewGroup parentViewGroup = (ViewGroup) view.getParent();
+            if (parentViewGroup != null) {
+                parentViewGroup.removeAllViews();
+            }
+        }
+    }
+
+    private LinearLayout makeGallery(int userID) {
+        LinearLayout linear = new LinearLayout(this.getContext());
 
         // region TESTING
 
@@ -38,15 +56,16 @@ public class GalleryFragment extends Fragment {
         @SuppressLint("StaticFieldLeak")
         ImageView im = new ImageView(c);
 
+
         for (int i = 0; i < 4; i++) {
             Glide.with(this)
                     .load("https://moodle.htwchur.ch/pluginfile.php/124614/mod_page/content/4/example.jpg")
                     .into(im);
-            gallery.addView(im, imParams);
+            linear.addView(im, imParams);
         }
 
         // endregion
 
-        return view;
+        return linear;
     }
 }
