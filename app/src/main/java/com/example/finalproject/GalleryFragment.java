fe50a88a54995fca297b3f26b35d0662b4fee880
roskomj <jordan.roskom@yahoo.com>
2019-02-25 13:12:34 -0600

    Glide works

--- app/src/main/java/com/example/finalproject/GalleryFragment.java
+++ app/src/main/java/com/example/finalproject/GalleryFragment.java
@@ -1,71 +1,51 @@
 package com.example.finalproject;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import com.bumptech.glide.Glide;
 
 public class GalleryFragment extends Fragment {
 
-    Context c;
-    LinearLayout gallery;
-    View view;
-
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        view = inflater.inflate(R.layout.fragment_gallery, container, false);
-        c = this.getContext();
-        gallery = view.findViewById(R.id.LinearP);
-
-        gallery.addView(makeGallery(55));
-
-        return view;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        if (view != null) {
-            ViewGroup parentViewGroup = (ViewGroup) view.getParent();
-            if (parentViewGroup != null) {
-                parentViewGroup.removeAllViews();
-            }
-        }
-    }
-
-    private LinearLayout makeGallery(int userID) {
-        LinearLayout linear = new LinearLayout(this.getContext());
-
-        // region TESTING
+        // View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+        // LinearLayout gallery = view.findViewById(R.id.LinearC);
 
         LinearLayout.LayoutParams imParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        @SuppressLint("StaticFieldLeak")
-        ImageView im = new ImageView(c);
+        ScrollView sv = new ScrollView(getActivity());
+        LinearLayout linear = new LinearLayout(getActivity());
+        linear.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout DAD = new LinearLayout(getActivity());
+        DAD.setOrientation(LinearLayout.VERTICAL);
 
-
+        // TODO Replace 4 with amount of foind images for current user
         for (int i = 0; i < 4; i++) {
+            ImageView im = new ImageView(getActivity());
             Glide.with(this)
+                    // TODO Replace URL with location of user's photo(s)
                     .load("https://moodle.htwchur.ch/pluginfile.php/124614/mod_page/content/4/example.jpg")
                     .into(im);
             linear.addView(im, imParams);
+            Log.i("TEST", String.valueOf(i));
         }
 
-        // endregion
+        sv.addView(linear);
+        DAD.addView(sv);
 
-        return linear;
+        return DAD;
     }
 }
