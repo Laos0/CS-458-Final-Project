7b95a17f8c0e0310b1a9bdeb690a69d70b1c130f
roskomj <jordan.roskom@yahoo.com>
2019-03-08 11:40:49 -0600

    Async the image GETs

--- app/src/main/java/com/example/finalproject/GalleryFragment.java
+++ app/src/main/java/com/example/finalproject/GalleryFragment.java
@@ -1,12 +1,18 @@
 package com.example.finalproject;
 
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,8 +24,23 @@ import android.widget.ScrollView;
 
 import com.bumptech.glide.Glide;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class GalleryFragment extends Fragment {
 
+    public static Bitmap image;
+    @SuppressLint("StaticFieldLeak")
+    public static ImageView im;
+    public static GridLayout.LayoutParams param = new GridLayout.LayoutParams();
+    @SuppressLint("StaticFieldLeak")
+    public static ScrollView DAD;
+    @SuppressLint("StaticFieldLeak")
+    public static LinearLayout linear;
+    @SuppressLint("StaticFieldLeak")
+    public static GridLayout grid;
+    @SuppressLint("StaticFieldLeak")
+    public static Activity a;
     String[] eatFoodyImages = {
             "http://i.imgur.com/rFLNqWI.jpg",
             "http://i.imgur.com/C9pBVt7.jpg",
@@ -42,44 +63,69 @@ public class GalleryFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        // View view = inflater.inflate(R.layout.fragment_gallery, container, false);
-        // LinearLayout gallery = view.findViewById(R.id.LinearC);
+        a = getActivity();
 
-        ScrollView DAD = new ScrollView(getActivity());
+        DAD = new ScrollView(getActivity());
         DAD.setSmoothScrollingEnabled(true);
         DAD.setHorizontalScrollBarEnabled(true);
 
-        LinearLayout linear = new LinearLayout(getActivity());
+        linear = new LinearLayout(getActivity());
         linear.setOrientation(LinearLayout.VERTICAL);
 
-        GridLayout grid = new GridLayout(getActivity());
-        grid.setColumnCount(2);
+        grid = new GridLayout(getActivity());
+        grid.setColumnCount(3);
         grid.setAlignmentMode(GridLayout.ALIGN_BOUNDS);
         grid.getUseDefaultMargins();
 
-
         // TODO Replace # with amount of found images for current user
-        for (int i = 0, c = 0, r = 0; i < eatFoodyImages.length; i++, c++) {
-            ImageView im = new ImageView(getActivity());
-            im.setScaleType(ImageView.ScaleType.FIT_XY);
-            GridLayout.LayoutParams param = new GridLayout.LayoutParams();
-            // GridLayout.LayoutParams.WRAP_CONTENT
-            param.height = 400;
-            param.width = 200;
-            param.rightMargin = 5;
-            param.topMargin = 5;
-            param.setGravity(Gravity.CENTER);
-            param.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
-            param.rowSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
-            Glide.with(this)
-                    // TODO Replace URL with location of user's photo(s)
-                    .load(eatFoodyImages[i])
-                    .into(im);
-            grid.addView(im, param);
+        for (String eatFoodyImage : eatFoodyImages) {
+            try {
+                URL url = new URL(eatFoodyImage);
+                new Async().execute(url);
+            } catch (IOException e) {
+                Log.i("TEST", e.toString());
+            }
         }
+
         linear.addView(grid);
         DAD.addView(linear);
 
         return DAD;
+    }
+}
+
+class Async extends AsyncTask<URL, Integer, Bitmap> {
+
+    @Override
+    protected Bitmap doInBackground(URL... urls) {
+        try {
+            return BitmapFactory.decodeStream(urls[0].openConnection().getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        // super.onPostExecute(bitmap);
+
+        GalleryFragment.im.setScaleType(ImageView.ScaleType.FIT_XY);
+        // GridLayout.LayoutParams.WRAP_CONTENT
+        // param.height = 200;
+        // param.width = 400;
+        GalleryFragment.param.rightMargin = 5;
+        GalleryFragment.param.topMargin = 5;
+        GalleryFragment.param.setGravity(Gravity.CENTER);
+        GalleryFragment.param.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
+        GalleryFragment.param.rowSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
+        Glide.with(GalleryFragment.a)
+                // TODO Replace URL with location of user's photo(s)
+                .load(Bitmap.createScaledBitmap(GalleryFragment.image, 120, 120, false))
+                .into(GalleryFragment.im);
+        GalleryFragment.grid.addView(GalleryFragment.im, GalleryFragment.param);
+
+        GalleryFragment.image = bitmap;
     }
 }
