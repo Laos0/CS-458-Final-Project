85cbd9eec9dccdbfc55b0f0371b512c63d9fb1db
Kase Radtke <Radtkek4507@my.uwstout.edu>
2019-04-17 12:58:05 -0500

    Removes weather button from apps mainpage (HomeFragment.java)

--- app/src/main/java/com/example/finalproject/HomeFragment.java
+++ app/src/main/java/com/example/finalproject/HomeFragment.java
@@ -77,7 +77,9 @@ public class HomeFragment extends Fragment
 
             }
         });
-        final Button weatherButton = view.findViewById(R.id.weather_btn);
+
+        // *************DELETE ME: Button for Nicks Weather Button. Can be deleted in future.**********************
+        /*final Button weatherButton = view.findViewById(R.id.weather_btn);
         weatherButton.setOnClickListener(new View.OnClickListener(){
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
@@ -86,7 +88,7 @@ public class HomeFragment extends Fragment
                 startActivity(WeatherPage);
 
             }
-        });
+        }); */
 
         // a boolean is returned from the MainPage
        photoExist = ((MainPage)getActivity()).isThereTargetPhoto();
